2d14fcdb997d146de1f876ebb61688957ea6f330
jjuandiego099 <jchaparro167@unab.edu.co>
2026-05-01 21:49:27 -0500

    Fix: limpiar cache en startup

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# Crear directorios necesarios
+mkdir -p /var/www/storage/framework/sessions
+mkdir -p /var/www/storage/framework/views
+mkdir -p /var/www/storage/framework/cache
+
+# Limpiar caché primero
+php artisan cache:clear
+php artisan config:clear
+php artisan view:clear
+
 # Optimizar Laravel
 php artisan config:cache
 php artisan route:cache
